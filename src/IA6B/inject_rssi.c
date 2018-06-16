2f87f0ab32351f812ca38883772807146e646ca6
Cleric-K <9365881+Cleric-K@users.noreply.github.com>
2018-06-16 10:44:32 +0300

    Fixed channel value for invalid SigStr (0xffff)

--- src/IA6B/inject_rssi.c
+++ src/IA6B/inject_rssi.c
@@ -1,10 +1,13 @@
+#define OFFSET_CHANNEL_14 13
+
 unsigned int get_signal_strength();
+
 
 void inject_rssi (unsigned short *buf) {
     int ss = get_signal_strength(); // ss varies from 0 (full signal) to 100 (worst)
 
     if(ss == 0xffff)
-        buf[13] = 0;
+        buf[OFFSET_CHANNEL_14] = 1000;
     else
-        buf[13] = 1000 + (100 - ss)*10; // scale to channel value 1000; 2000
+        buf[OFFSET_CHANNEL_14] = 1000 + (100 - ss)*10; // scale to channel value [1000; 2000]
 }
